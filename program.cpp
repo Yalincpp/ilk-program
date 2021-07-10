97aed06cec88efefe4cad9100c26d5132bbac463
Yalin Ince <yalinince8@gmail.com>
2021-07-10 13:43:36 +0300

    ilk program dosyasini ekle.

---
+++ program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main ()
+{
+std::cout << " EN SEVİLEN MEYVELER" << std::endl;
+std::cout << " ||               " << std::endl;
+std::cout << " ||             ||" << std::endl;
+std::cout << " ||      ||     ||" << std::endl;
+std::cout << "ELMA   ARMUT   MUZ" << std::endl;
+    
+    return 0;
+}
